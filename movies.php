4e418c6796638a06dc2c913b747aad9cd13a1be5
Andika Dwiyanto <95038983+andik71@users.noreply.github.com>
2023-06-02 20:57:50 +0800

    final_project

---
+++ movies.php
@@ -0,0 +1,184 @@
+<section class="after-head d-flex section-text-white position-relative">
+    <div class="d-background" data-image-src="http://via.placeholder.com/1920x1080" data-parallax="scroll"></div>
+    <div class="d-background bg-black-80"></div>
+    <div class="top-block top-inner container">
+        <div class="top-block-content">
+            <h1 class="section-title">Gallery</h1>
+            <div class="page-breadcrumbs">
+                <a class="content-link" href="#">Home</a>
+                <span class="text-theme mx-2"><i class="fas fa-chevron-right"></i></span>
+                <span>Gallery</span>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-long">
+    <div class="container">
+        <div class="grid row">
+            <div class="col-sm-6 col-lg-4">
+                <div class="gallery-entity">
+                    <div class="entity-preview" data-role="hover-wrap">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                        </div>
+                        <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                            <div class="entity-view-popup">
+                                <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
+                                    <span class="icon-content"><i class="fas fa-search"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="entity-title">The image title</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="gallery-entity">
+                    <div class="entity-preview" data-role="hover-wrap">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                        </div>
+                        <div class="bg-theme-lighted d-over collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                            <div class="entity-view-popup">
+                                <a class="action-icon-theme action-icon-bordered rounded-circle" href="https://www.youtube.com/watch?v=d96cjJhvlMA" data-magnific-popup="iframe">
+                                    <span class="icon-content"><i class="fas fa-play"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="entity-title">The video title</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="gallery-entity">
+                    <div class="entity-preview" data-role="hover-wrap">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                        </div>
+                        <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                            <div class="entity-view-popup">
+                                <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
+                                    <span class="icon-content"><i class="fas fa-search"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="entity-title">The image title</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="gallery-entity">
+                    <div class="entity-preview" data-role="hover-wrap">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                        </div>
+                        <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                            <div class="entity-view-popup">
+                                <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
+                                    <span class="icon-content"><i class="fas fa-search"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="entity-title">The image title</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="gallery-entity">
+                    <div class="entity-preview" data-role="hover-wrap">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                        </div>
+                        <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                            <div class="entity-view-popup">
+                                <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
+                                    <span class="icon-content"><i class="fas fa-search"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="entity-title">The image title</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="gallery-entity">
+                    <div class="entity-preview" data-role="hover-wrap">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                        </div>
+                        <div class="bg-theme-lighted d-over collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                            <div class="entity-view-popup">
+                                <a class="action-icon-theme action-icon-bordered rounded-circle" href="https://www.youtube.com/watch?v=d96cjJhvlMA" data-magnific-popup="iframe">
+                                    <span class="icon-content"><i class="fas fa-play"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="entity-title">The video title</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="gallery-entity">
+                    <div class="entity-preview" data-role="hover-wrap">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                        </div>
+                        <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                            <div class="entity-view-popup">
+                                <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
+                                    <span class="icon-content"><i class="fas fa-search"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="entity-title">The image title</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="gallery-entity">
+                    <div class="entity-preview" data-role="hover-wrap">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                        </div>
+                        <div class="bg-theme-lighted d-over collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                            <div class="entity-view-popup">
+                                <a class="action-icon-theme action-icon-bordered rounded-circle" href="https://www.youtube.com/watch?v=d96cjJhvlMA" data-magnific-popup="iframe">
+                                    <span class="icon-content"><i class="fas fa-play"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="entity-title">The video title</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="gallery-entity">
+                    <div class="entity-preview" data-role="hover-wrap">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                        </div>
+                        <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                            <div class="entity-view-popup">
+                                <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
+                                    <span class="icon-content"><i class="fas fa-search"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="entity-title">The image title</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section-bottom">
+            <div class="paginator">
+                <a class="paginator-item" href="#"><i class="fas fa-chevron-left"></i></a>
+                <a class="paginator-item" href="#">1</a>
+                <span class="active paginator-item">2</span>
+                <a class="paginator-item" href="#">3</a>
+                <span class="paginator-item">...</span>
+                <a class="paginator-item" href="#">10</a>
+                <a class="paginator-item" href="#"><i class="fas fa-chevron-right"></i></a>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ navbar.php
@@ -0,0 +1,123 @@
+<body class="body">
+        <header class="header header-horizontal header-view-pannel">
+            <div class="container">
+                <nav class="navbar">
+                    <a class="navbar-brand" href="./">
+                        <span class="logo-element">
+                            <span class="logo-tape">
+                                <span class="svg-content svg-fill-theme" data-svg="./images/svg/logo-part.svg"></span>
+                            </span>
+                            <span class="logo-text text-uppercase">
+                                <span>M</span>emico</span>
+                        </span>
+                    </a>
+                    <button class="navbar-toggler" type="button">
+                        <span class="th-dots-active-close th-dots th-bars">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </button>
+                    <div class="navbar-collapse">
+                        <ul class="navbar-nav">
+                            <li class="nav-item nav-item-arrow-down nav-hover-show-sub">
+                                <a class="nav-link" href="index.php?page=home">Homepage</a>
+                                <!-- <div class="nav-arrow"><i class="fas fa-chevron-down"></i></div>
+                                <ul class="collapse nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="index.php?page=home">Homepage 1</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="homepage-2.html">Homepage 2</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="homepage-3.html">Homepage 3</a>
+                                    </li>
+                                </ul> -->
+                            </li>
+                            <li class="nav-item nav-item-arrow-down nav-hover-show-sub">
+                                <a class="nav-link" href="#" data-role="nav-toggler">Pages</a>
+                                <div class="nav-arrow"><i class="fas fa-chevron-down"></i></div>
+                                <ul class="collapse nav">
+                                    <li class="nav-item nav-item-arrow-down nav-hover-show-sub">
+                                        <a class="nav-link" href="#" data-role="nav-toggler">Movies</a>
+                                        <div class="nav-arrow"><i class="fas fa-chevron-down"></i></div>
+                                        <ul class="collapse nav">
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="movies-blocks.html">Blocks - No Sidebar</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="movies-blocks-sidebar-right.html">Blocks - Sidebar right</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="movies-posters.html">Posters - No Sidebar</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="movies-posters-sidebar-right.html">Posters - Sidebar right</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="movies-list.html">List - No Sidebar</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="movie-info-sidebar-right.html">Movie info</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="gallery.html">Gallery</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="news-blocks-sidebar-right.html">News</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="article-sidebar-right.html">Article</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="about-us.html">About us</a>
+                                    </li>
+                                    <li class="nav-item nav-item-arrow-down nav-hover-show-sub">
+                                        <a class="nav-link" href="#" data-role="nav-toggler">User pages</a>
+                                        <div class="nav-arrow"><i class="fas fa-chevron-down"></i></div>
+                                        <ul class="collapse nav">
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="sign-in.html">Sign in</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="sign-up.html">Sign up</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item nav-item-arrow-down nav-hover-show-sub">
+                                        <a class="nav-link" href="#" data-role="nav-toggler">Status pages</a>
+                                        <div class="nav-arrow"><i class="fas fa-chevron-down"></i></div>
+                                        <ul class="collapse nav">
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="under-construction.html">Under construction</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="coming-soon.html">Coming soon</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="404-1.html">404 - 1</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="404-2.html">404 - 2</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="movies-blocks.html">Movies</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact-us.html">Contact us</a>
+                            </li>
+                        </ul>
+                        <div class="navbar-extra">
+                            <a class="btn-theme btn" href="#"><i class="fas fa-ticket-alt"></i>&nbsp;&nbsp;Buy Ticket</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>

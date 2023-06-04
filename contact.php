fe19d510daf16de2409ccbf49ec47199e85b63bf
rizkiaulia <auliarizki0970@gmail.com>
2023-06-04 23:57:35 +0700

    Update contact

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
-<section class="after-head d-flex section-text-white position-relative" style="background-image: url('images/image1.png');" >
+<section class="after-head d-flex section-text-white position-relative" style="background-image: url('images/image1.png');">
     <div class="d-background bg-black-50"></div>
     <div class="top-block top-inner container">
         <div class="top-block-content">
@@ -13,43 +13,43 @@
 </section>
 <section>
     <div class="gmap-with-map bg-white">
-                <div class="gmap" data-lat="-50.878897" data-lng="151.103737">
-                    <iframe style="border:0; width: 100%; height: 500px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d493.94212418348434!2d112.61591113524486!3d-7.9433266018527355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e788274e9c41b51%3A0xb66aa2d4fd7ab2e5!2sStudio%20Rupa%20Malang%20Store!5e0!3m2!1sid!2sid!4v1685096592149!5m2!1sid!2sid" frameborder="0" allowfullscreen></iframe>
-                </div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6 ml-lg-auto">                 
-                            <div class="gmap-form bg-white">
-                                <h4 class="form-title text-uppercase">Contact
-                                    <span class="text-theme">us</span>
-                                </h4>
-                                <p class="form-text">We understand your requirement and provide quality works</p>
-                                <form autocomplete="off">
-                                    <div class="row form-grid">
-                                        <div class="col-sm-6">
-                                            <div class="input-view-flat input-group">
-                                                <input class="form-control" name="name" type="text" placeholder="Name" />
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="input-view-flat input-group">
-                                                <input class="form-control" name="email" type="email" placeholder="Email" />
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <div class="input-view-flat input-group">
-                                                <textarea class="form-control" name="message" placeholder="Message"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <button class="px-5 btn btn-theme" type="submit">Send</button>
-                                        </div>
+        <div class="gmap" data-lat="-50.878897" data-lng="151.103737">
+            <iframe style="border:0; width: 100%; height: 500px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d493.94212418348434!2d112.61591113524486!3d-7.9433266018527355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e788274e9c41b51%3A0xb66aa2d4fd7ab2e5!2sStudio%20Rupa%20Malang%20Store!5e0!3m2!1sid!2sid!4v1685096592149!5m2!1sid!2sid" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 ml-lg-auto">
+                    <div class="gmap-form bg-white">
+                        <h4 class="form-title text-uppercase">Contact
+                            <span class="text-theme">us</span>
+                        </h4>
+                        <p class="form-text">We understand your requirement and provide quality works</p>
+                        <form autocomplete="off">
+                            <div class="row form-grid">
+                                <div class="col-sm-6">
+                                    <div class="input-view-flat input-group">
+                                        <input class="form-control" name="name" type="text" placeholder="Name" />
                                     </div>
-                                </form>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="input-view-flat input-group">
+                                        <input class="form-control" name="email" type="email" placeholder="Email" />
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="input-view-flat input-group">
+                                        <textarea class="form-control" name="message" placeholder="Message"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button class="px-5 btn btn-theme" type="submit">Send</button>
+                                </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
+            </div>
+        </div>
     </div>
 </section>
 <section class="section bg-white text-center">

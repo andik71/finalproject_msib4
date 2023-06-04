f19cf5732ddffdf0ca9f9cff7e7fca7cf4094688
rizkiaulia <auliarizki0970@gmail.com>
2023-06-04 20:41:27 +0700

    Detail actor

---
+++ actor.php
@@ -0,0 +1,75 @@
+<section class="after-head d-flex section-text-white position-relative" style="background-image: url('images/image1.png');">
+    <div class="d-background bg-black-50"></div>
+     <div class="top-block top-inner container">
+        <div class="top-block-content">
+            <h1 class="section-title">Song Joong Ki</h1>
+            <div class="page-breadcrumbs">
+                <a class="content-link" href="index.php?page=home">Home</a>
+                <span class="text-theme mx-2"><i class="fas fa-chevron-right"></i></span>
+                Biography
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bg-white p-5">
+    <div class="container">
+        <div class="section-head">
+            <h2 class="section-title text-uppercase text-dark">overview</h2>
+        </div>
+        <div class="section-description">
+            <div class="card p-4">
+                <div class="grid row">                 
+                    <div class="col-md-3">
+                    <img src="images/hidayah.jpg" alt="" srcset="" width="150px">
+                    </div>
+                    <div class="col-md-6 p-4">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <span class="font-weight-bold text-dark"><b>Born</b></span>
+                            </div>
+                            <div class="col-md-4">
+                                September 19, 1985
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <span class="font-weight-bold text-dark">Active Year</span>
+                            </div>
+                            <div class="col-md-4">
+                               2008 - present
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <span class="font-weight-bold text-dark">Agen</span>
+                            </div>
+                            <div class="col-md-4">
+                               History
+                            </div>
+                        </div>
+                    </div>
+               </div>                           
+            </div>
+        </div>
+        
+        <br>
+        <div class="section-head">
+            <h2 class="section-title text-uppercase text-dark">mini bio</h2>
+        </div>
+        <div class="card section-description p-5">
+            <p class="lead">Song Joong-ki (Korean: 송중기; born September 19, 1985) is a South Korean actor. 
+                He rose to fame in the historical coming-of-age drama Sungkyunkwan Scandal (2010) and the variety show Running Man (2010–2011) 
+                as one of the original cast members. Since then, he has played a diverse spectrum of roles in the television series 
+                The Innocent Man (2012), Descendants of the Sun (2016), Arthdal Chronicles (2019), Vincenzo (2021), and Reborn Rich (2022), 
+                as well as the hit films A Werewolf Boy (2012), The Battleship Island (2017), and Space Sweepers (2021).</p>
+            <p class="lead">
+                Song was Gallup Korea's Television Actor of the Year in 2012 and in 2017. He was first included in the Forbes Korea Power Celebrity list 
+                in 2013 placing seventh, and subsequently ranking second in 2017, and seventh in 2018. The success of his TV series and film works 
+                internationally established him as a top Hallyu star in the Korean entertainment industry.
+            </p>                   
+        </div>
+        <br>
+    </div>
+</section>

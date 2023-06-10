c30f6f67ec92a56aa15533d10b148f9a94abfcad
rizkiaulia <auliarizki0970@gmail.com>
2023-06-10 11:07:48 +0700

    Update About

--- about.php
+++ about.php
@@ -12,29 +12,66 @@
   </div>
 </section>
 
-
 <section class="bg-white p-5">
   <div class="container">
     <div class="section-head">
       <h2 class="section-title text-uppercase text-dark">About Us</h2>
     </div>
     <div class="section-description">
-      <div class="card p-4">
+      <div class="card p-3">
         <div class="grid row">
           <div class="col-md-3">
             <img src="images/gif.gif" alt="" srcset="" width="250px">
           </div>
-          <div class="col-md-6 p-4 m-4">
-            <h1>Film tak terbatas dan masih banyak terbaru</h1>
-            <h3>Tonton dimana pun, dan kapanpun</h3>
-
+          <div class="col-md-8">
+            <p align="left" style="letter-spacing: 2px; line-height: 30px; font-weight:bold; text-align:justify;" class="text-dark px-5">Film Kita merupakan sebuah web movie, sering dikenal sebagai situs streaming film, adalah platform online di mana pengguna dapat menonton film melalui internet. Web movie telah menjadi salah satu cara yang populer bagi orang untuk mengakses konten hiburan secara online.</p>
           </div>
         </div>
       </div>
     </div>
-
     <br>
-
     <br>
   </div>
+</section>
+<section class="bg-white">
+  <div class="container">
+    <div class="section-head">
+      <h2 class="section-title text-uppercase text-dark">The Team</h2>
+    </div>
+    <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
+      <div class="col">
+        <div class="card shadow-lg">
+          <img src="images/svg/male.jpg" width="20%" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-dark font-weight-bold text-center">Andhika</h5>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card shadow-lg">
+          <img src="images/svg/female.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-dark font-weight-bold text-center">Aulia</h5>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-lg">
+          <img src="images/svg/female.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-dark font-weight-bold text-center">Faidatul</h5>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-lg">
+          <img src="images/svg/male.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-dark font-weight-bold text-center">Rifky</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br><br>
 </section>
